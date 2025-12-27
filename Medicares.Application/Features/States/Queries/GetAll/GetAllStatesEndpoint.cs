b661e5f32b59bec3712b05ac60524849db737c28
manish kumar <yadavmanishk2511@gmail.com>
2025-12-27 07:08:25 -0800

    get  all state end point added

---
+++ Medicares.Application/Features/States/Queries/GetAll/GetAllStatesEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using FastEndpoints;
+using Medicares.Application.Contracts.Interfaces.Repositories;
+using Medicares.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medicares.Application.Features.States.Queries.GetAll
+{
+    public class GetAllStatesEndpoint(IUnitOfWork unitOfWork) : EndpointWithoutRequest<List<GetAllStatesResponse>>
+    {
+        public override void Configure()
+        {
+            Get(StatesApiRoutes.GetAllStates);
+
+            AllowAnonymous();
+
+            Summary(s =>
+            {
+                s.Summary = StatesApiRoutes.Summary;
+            });
+
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            List<State> states = await unitOfWork.Repository<State>().Entities.ToListAsync(ct);
+            await SendOkAsync(states.MapToGetAllStatesQueryResponse(), ct);
+        }
+    }
+}
---
+++ Medicares.Application/Features/States/Queries/GetAll/GetAllStatesMapper.cs
@@ -0,0 +1,16 @@
+﻿using Medicares.Domain.Entities.Common;
+namespace Medicares.Application.Features.States.Queries.GetAll
+{
+    public static class GetAllStatesMapper
+    {
+        public static List<GetAllStatesResponse> MapToGetAllStatesQueryResponse(this IEnumerable<State> responses)
+        {
+            return responses.Select(response => new GetAllStatesResponse
+            {
+                Id = response.Id,
+                Name = response.Name,
+                ShortName = response.ShortName,
+            }).ToList();
+        }
+    }
+}
