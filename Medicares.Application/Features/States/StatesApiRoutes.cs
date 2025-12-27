4cdc6a351881335ab4c313717350381890df4a9c
manish kumar <yadavmanishk2511@gmail.com>
2025-12-27 07:08:25 -0800

    get  all state end point added

---
+++ Medicares.Application/Features/States/StatesApiRoutes.cs
@@ -0,0 +1,10 @@
+﻿using FastEndpoints;
+
+namespace Medicares.Application.Features.States
+{
+    internal  class StatesApiRoutes:Group
+    {
+        public const string GetAllStates = "state/get-all";
+        public const string Summary = "Get All States";
+    }
+}
